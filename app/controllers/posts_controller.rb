52421d5c84b36e51a8cdd92ac07045fab14e6800
James Benjamin Pawlik <james.benjamin.pawlik@gmail.com>
2021-08-16 15:15:31 -0700

    prevent user from deleting posts by other users
    Co-authored-by: dqveha <dqveha@users.noreply.github.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,6 @@ class PostsController < ApplicationController
     json_response(@posts)
   end
 
-  def search_by_time
-    @posts = Post.timeframe(begin, end)
-    json_response(@posts)
-  end
-
   def show
     @post = Post.find(params[:id])
     json_response(@post)
@@ -25,25 +20,35 @@ class PostsController < ApplicationController
   def update
     @board = Board.find(params[:board_id])
     @post = Post.find(params[:id])
-    if @post.update!(post_params)
+    @post2 = Post.update(post_params)
+    if @post.username === @post2.username && @post.author === @post2.author
+      # @post.update!(post_params)
+      @post = Post.update
       render status: 200, json: {
         message: "This post has been updated successfully"
+      }
+    else
+      render status: 403, json: {
+        message: "Post not updated; confirm that you are the author"
       }
     end
   end
 
   def destroy
     @post = Post.find(params[:id])
-    if @post.destroy
+    if @post.username == @post.author && @post.delete
       render status: 200, json: {
         message: "This post has been successfully deleted"
+      }
+    else
+      render status: 403, json: {
+        message: "Unable to delete post; confirm that you are the author"
       }
     end
   end
 
-
-  private 
+  private
     def post_params
-      params.permit(:title, :body, :board_id)
+      params.permit(:title, :body, :username, :author, :board_id)
     end
 end
